da44f406168244ebe522b74aafed171c3a19509f
mahi <mohiddinoshim@yahoo.com>
2020-06-07 16:53:14 +0600

    scrolling to Elements

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,26 +1,44 @@
 $(document).ready(function() {
-	
+
+	/* Sticky navigation */
 	$('.js--section-features').waypoint(function(direction) {
 		if (direction == "down") {
 			$('nav').addClass('sticky');	
 		} else {
 			$('nav').removeClass('sticky');
-
 		}
-
 	 {
   			offset: '60px'
 				}
+}); 
+
+
+		/* Scroll on buttons */
+	
+
+	$('.js--scroll-to-plans').click(function () {
+		$('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+	});
+
+	$('.js--scroll-to-start').click(function () {
+			$('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+		});
 
 
 
-	}); 
 
-	/*
-	 var waypoints = $('#handler-first').waypoint(function(direction) {
-  		notify(this.element.id + ' hit 25% from top of window') 
-		}, {
-  			offset: '25%'
-				})
-	*/
+     /* Navigation on scroll */
+
+
+
+
+     /*Animation on scroll */
+
+     $('.js--wp-1').waypoint(function (direction) {
+     	$('.js--wp-1').addClass('animated fadeIn')
+     }, {
+     	offset: '50%'
+
+     });
+
 });
